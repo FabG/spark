6196392364f67b119bfbf45a54e068e10fa92018
FabG <fabrice.guillaume@circleback.com>
2016-01-03 09:39:20 -0500

    filtering input file based on record type

--- changelogs-poc/src/main/scala/parse_cl_poc.scala
+++ changelogs-poc/src/main/scala/parse_cl_poc.scala
@@ -5,41 +5,61 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-
+import java.text.SimpleDateFormat
+import scala.util.{Try, Success, Failure}
+ 
 object parseCL {
   def main(args: Array[String]) {
-    /* Input file (ChangeLogs TSV file) */
-    val changeLogsFile = "./input/users.contact-changes.1000-users_sample10.tsv"
-
-    val outputFile = "./output/parsed.contact-changes_sample10.tsv"
-
     /* Spark Conf and Spark Context */
     val conf = new SparkConf().setAppName("Change Logs Parser POC")
     val sc = new SparkContext(conf)
-    /* cache RDD  */
-    val changeLogsData = sc.textFile(changeLogsFile, 2).cache()
 
-    /* Transformations */
-    val changeLogsParse = changeLogsData.map(line => line.split("\t"))
+    /* Input file (ChangeLogs TSV file) */
+    val InputFile = "./input/users.contact-changes.1000-users-sample100.tsv"
 
-    /* Filter source = 600 */
-    val filteredSource = changeLogsParse.filter(line => line(10).equals("600"))
+    /* Input Data RDD (Resilient Distributed Dataset) */
+    val inputData = sc.textFile(InputFile)
+
+    /* Output File */
+    val outputFile = "./output/parsed.contact-changes_sample10.tsv"
 
     /* Keep only the Record types of interest */
     /* 1=Name, 2=Address, 3=Email, 6=Phone Number, 13=Job Info */
-    /* RecordType is 3rd in our TSV file (2nd in our RDD as it's an array) */
-    
-    val numNames = filteredSource.filter(line => line(2).equals("1")).count()
-    val numEmails = filteredSource.filter(line => line(2).equals("3")).count()
-    val numPhones = filteredSource.filter(line => line(2).equals("6")).count()
-    val numJobinfo = filteredSource.filter(line => line(2).equals("13")).count()
-    val numAddress = filteredSource.filter(line => line(2).equals("2")).count()
+    /* Creating this RDD parallelizing the collection of record types */
+    val splitInputData = inputData.map(_.split("\t"))
+    val filteredRecTypeData = splitInputData.filter(line => 
+      line(2).equals("1") ||
+      line(2).equals("2") ||
+      line(2).equals("3") ||
+      line(2).equals("6") ||
+      line(2).equals("13") )
+  
+    val numNames = filteredRecTypeData.filter(line => line(2).equals("1")).count()
+    val numEmails = filteredRecTypeData.filter(line => line(2).equals("3")).count()
+    val numPhones = filteredRecTypeData.filter(line => line(2).equals("6")).count()
+    val numJobinfo = filteredRecTypeData.filter(line => line(2).equals("13")).count()
+    val numAddress = filteredRecTypeData.filter(line => line(2).equals("2")).count()
 
-    /* Actions */
+    println("Out of the following original contacts: %s, After filtering based on Record Types 1,2,3,6,13, we now have: %s".format(splitInputData.count(), filteredRecTypeData.count()))
     println("Lines with name: %s, Lines with email: %s, Lines with phone: %s, Lines with JobInfo: %s, Lines with Address: %s".format(numNames, numEmails, numPhones, numJobinfo, numAddress))
 
+    /* Actions */
+
    /* Export to file the filtered lines */
-   filteredSource.saveAsTextFile(outputFile)
+   filteredRecTypeData.saveAsTextFile(outputFile)
+
+
+
+    /* Transformation: only keep fields of value */
+    /* 0=cb_contact_id, 1=cb_user_id, 2=cb_user_id, 3=cb_timestamp */
+    /* 5=created_on, 6=modified_on, 8=change_type, 9=item_type */
+    /* 10=source, 12=raw_col_1(All,FN,Org,Street1), 13=raw_col_2(LN,Title,Street2) */
+    /* 14=raw_col_3(City), 15=raw_col_4(Zip), 16=raw_col_5(State) */
+    /* 17=raw_col_6(Country) */
+    /* Plus some normalized fields: */
+    /* 23=(All,FN,Org,Street1), 24=(LN,Title,Street2), 25=Phone, 26=PhoneExtension */
+    /* 27=first6 Digits Phone, 28=Location */
+
 
    
   }
